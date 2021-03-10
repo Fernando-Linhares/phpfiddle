7ab829835c75e660fe14d400e65ef1ab20d14863
Fernando Linhares Silvestre <73559240+Fernando-Linhares@users.noreply.github.com>
2021-03-10 10:00:13 -0300

    Update home.php

--- phpfiddle/resources/views/home.php
+++ phpfiddle/resources/views/home.php
@@ -9,39 +9,41 @@
 	<title>Home</title>
 </head>
 <body style="background-image: url('assets/images/background.png'); background-size: 100%;">
-	<div class="card-panel">
-		<h3 class="blue-text text-darken-2">Phpfiddle</h3>
-	</div>
-
-	<form class="card-panel white" id="form" method="post" action="/exec" >
-	  <div class="row">
-
-	        <div class="input-field col s6">
- 			 <i class="material-icons prefix">code</i>
-	          <textarea id="textarea1" name="code" class="materialize-textarea" style="height: 300px;color: rgb(0,0,100);"></textarea>
-	        </div>
-	        <div class="col s2">
-	  			<button type="submit" class="btn-floating blue pull-6"><i class="material-icons">play_circle_outline</i></button> 
-	  		</div>
-	  		<div class="col s6">
-	  			<div class="card white" style="height: 250px;">
-		  			<hr>
-					<h6 id="result" class="card-content"></h6>
-					<hr>
-	  			</div>
-	  		</div>
-	  </div>
-	</form>
-
-	<footer>
-		<div class="card-panel white">
-			<div class="row">
-				<div class="col"><a href="https://www.php.net/">documentation</a></div>
-				<div class="col"><a href="https://github.com/Fernando-Linhares">github</a></div>
-				<div class="col"><a href="https://github.com/Fernando-Linhares/portfolio-dev/">portifolio</a></div>
-			</div>
+	<div class="container">
+		<div class="card-panel" style="padding-top: 20px;!important" >
+			<h3 class="blue-text text-darken-2">Phpfiddle</h3>
 		</div>
-	</footer>
+
+		<form class="card-panel white" id="form" method="post" action="/exec" >
+		  <div class="row">
+
+		        <div class="input-field col s6">
+	 			 <i class="material-icons prefix">code</i>
+		          <textarea spellcheck="false" id="textarea1" name="code" class="materialize-textarea" style="height: 300px;color: rgb(0,0,100);"></textarea>
+		        </div>
+		        <div class="col s2">
+		  			<button type="submit" class="btn-floating blue pull-6"><i class="material-icons">play_circle_outline</i></button> 
+		  		</div>
+		  		<div class="col s6">
+		  			<div class="card white" style="height: 250px;">
+			  			<hr>
+						<h6 id="result" class="card-content"></h6>
+						<hr>
+		  			</div>
+		  		</div>
+		  </div>
+		</form>
+
+		<footer>
+			<div class="card-panel white">
+				<div class="row">
+					<div class="col"><a href="https://www.php.net/">documentation</a></div>
+					<div class="col"><a href="https://github.com/Fernando-Linhares">github</a></div>
+					<div class="col"><a href="https://github.com/Fernando-Linhares/portifolio-backend-dev">portifolio</a></div>
+				</div>
+			</div>
+		</footer>
+	</div>
 
 	<script src="assets/js/app.js"></script>
 </body>
